82ed6bcec4da6a4d066e2f82128c30432d26f98f
Mac3 <92975145+mac-3@users.noreply.github.com>
2021-12-02 05:50:10 +0100

    D01P2 quality of code update

--- d01/p2/main.rs
+++ d01/p2/main.rs
@@ -2,36 +2,21 @@ use std::error::Error;
 use std::fs;
 use std::io::BufRead;
 
-const MEASURE_LENGTH: usize = 3;
+const WINDOW_SIZE: usize = 3;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read("input.txt")?
+    let (_, count) = fs::read("input.txt")?
         .lines()
         .filter_map(|r| r.ok())
         .filter_map(|n| n.parse::<u32>().ok())
-        .collect::<Vec<u32>>();
-
-    let mut v = &input[..];
-    let (mut last, mut count) = (None, 0u32);
-
-    while v.len() >= MEASURE_LENGTH {
-        // SAFETY: It is guaranted by the loop condition to not fail.
-        let curr = v[..MEASURE_LENGTH]
-            .iter()
-            .cloned()
-            .reduce(|a, b| a + b)
-            .unwrap();
-        v = &v[1..];
-        last = match last {
-            Some(last) => {
-                if last < curr {
-                    count += 1;
-                }
-                Some(curr)
-            }
-            None => Some(curr),
-        }
-    }
+        .collect::<Vec<u32>>()
+        .as_slice()
+        .windows(WINDOW_SIZE)
+        .map(|w| w.iter().cloned().reduce(|a, b| a + b))
+        .fold((None, 0u32), |(last, count), curr| match last {
+            Some(last) => (Some(curr), if last < curr { count + 1 } else { count }),
+            None => (Some(curr), count),
+        });
     println!("{}", count);
     Ok(())
 }
