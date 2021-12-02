d8e4e07b90122c490a0f10356fc5a3cebe691bc5
Mac3 <92975145+mac-3@users.noreply.github.com>
2021-12-02 06:53:56 +0100

    Day 2 Solution

---
+++ d02/p1/main.rs
@@ -0,0 +1,44 @@
+use std::io::BufRead;
+
+#[derive(Default)]
+struct Pos {
+    x: u32,
+    depth: u32,
+}
+
+enum Command {
+    Forward(u32),
+    Up(u32),
+    Down(u32),
+}
+
+impl Command {
+    fn parse(input: impl AsRef<str>) -> Option<Command> {
+        let mut s = input.as_ref().split_whitespace();
+        match s.next().unwrap() {
+            "forward" => Some(Command::Forward(s.next()?.parse::<u32>().ok()?)),
+            "up" => Some(Command::Up(s.next()?.parse::<u32>().ok()?)),
+            "down" => Some(Command::Down(s.next()?.parse::<u32>().ok()?)),
+            _ => None,
+        }
+    }
+
+    fn apply(&self, mut pos: Pos) -> Pos {
+        match self {
+            Command::Forward(u) => pos.x += u,
+            Command::Up(u) => pos.depth -= *u,
+            Command::Down(u) => pos.depth += *u,
+        };
+        pos
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = std::fs::read("../assets/input.txt")?
+        .lines()
+        .filter_map(|r| r.ok())
+        .filter_map(Command::parse)
+        .fold(Pos::default(), |pos, dir| dir.apply(pos));
+    println!("{}", input.x * input.depth);
+    Ok(())
+}
